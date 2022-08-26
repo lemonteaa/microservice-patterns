4fd5c045c372723318c5722696bdbd2a07365889
lemontea <lemontea.Tom@gmail.com>
2022-08-26 19:00:07 +0000

    toucan smoke test

--- quick-db/src/quick_db/db.clj
+++ quick-db/src/quick_db/db.clj
@@ -1,12 +1,13 @@
 (ns quick-db.db
   (:require [toucan.db :as tdb]
+            [toucan.models :refer [defmodel IModel]]
             [clojure.java.jdbc :as jdbc]
             [quick-db.kafka :as kafka]))
 
 (tdb/set-default-db-connection!
  {:classname   "org.postgresql.Driver"
   :subprotocol "postgresql"
-  :subname     "//localhost:5432/my_db"
+  :subname     "//localhost:5432/postgres"
   :user        "postgres"
   :password    "postgres"})
 
@@ -26,6 +27,19 @@
                :first_name "Rose", 
                :last_name "Mary", 
                :email "admin@co-iter.com"})
+
+;(defmodel PhoneNumber :phone_numbers
+;  IModel
+;  (primary-key [_] :number))
+
+(defmodel Customers :customers 
+  IModel 
+  (primary-key [_] :customer_id))
+
+(tdb/insert! Customers :customer_name "Tommy")
+(tdb/insert! Customers :customer_name "Mary")
+
+(Customers 2)
 
 (def worker (future (kafka/run-application "localhost:9092" 
                                            "dbserver1.inventory.customers" 
