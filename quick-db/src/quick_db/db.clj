db51462b6c7f85b7f45509530a5121c0d07c404c
lemontea <lemontea.Tom@gmail.com>
2022-08-28 20:02:51 +0000

    More ORM thingy

--- quick-db/src/quick_db/db.clj
+++ quick-db/src/quick_db/db.clj
@@ -1,6 +1,7 @@
 (ns quick-db.db
   (:require [toucan.db :as tdb]
             [toucan.models :refer [defmodel IModel]]
+            [toucan.hydrate :refer [hydrate] :as hydrate]
             [clojure.java.jdbc :as jdbc]
             [quick-db.kafka :as kafka]))
 
@@ -34,12 +35,82 @@
 
 (defmodel Customers :customers 
   IModel 
-  (primary-key [_] :customer_id))
+  (primary-key [_] :customer_id)
+  (hydration-keys [_]
+                  [:customer]))
 
 (tdb/insert! Customers :customer_name "Tommy")
 (tdb/insert! Customers :customer_name "Mary")
 
 (Customers 2)
+
+(defmodel StoreItem :store_item
+  IModel
+  (primary-key [_] :store_item_id)
+  (hydration-keys [_]
+                  [:store_item]))
+
+(defn add-store-item [name desc]
+  (tdb/insert! StoreItem 
+               :store_item_name name
+               :store_item_description desc))
+
+(add-store-item "Plush Toy" "Relax")
+(add-store-item "Bath Tub" "Large and shiny")
+(add-store-item "Spoon" "Spoon")
+(add-store-item "Shoe" "a pair and made from recycleables")
+(add-store-item "Mattress" "For winter")
+
+(defmodel Shipping :shipping
+  IModel
+  (primary-key [_] :shipping_id)
+  (hydration-keys [_]
+                  [:shipping]))
+
+(defmodel Order :order
+  IModel
+  (primary-key [_] :order_id))
+
+(defn create-empty-order [cust {:keys [contact phone email addr]}] 
+  (let [shipping (tdb/insert! Shipping
+                              :contact_name contact
+                              :phone phone
+                              :email email
+                              :shipping_address addr)]
+    (tdb/insert! Order
+                 :customer_id cust
+                 :shipping_id (:shipping_id shipping))))
+
+(create-empty-order 2 {:contact "Anderson" :phone "31152768"})
+
+(hydrate (Order 1) :customer :shipping)
+
+(defmodel OrderItem :order_item
+  IModel
+  (primary-key [_] :order_item_id))
+
+(defn add-item-to-order [order-id {:keys [id qty total-price]}]
+  (tdb/insert! OrderItem
+               :store_item_id id
+               :quantity qty
+               :total_price total-price
+               :order_id order-id))
+
+(add-item-to-order 1 {:id 3 :qty 4 :total-price 20})
+(add-item-to-order 1 {:id 5 :qty 1 :total-price 120})
+(add-item-to-order 1 {:id 1 :qty 3 :total-price 180})
+
+(tdb/select OrderItem :order_id 1)
+
+(defn ^:hydrate order-items [{:keys [order_id]}]
+  (tdb/select OrderItem :order_id order_id))
+
+; need vec for nested hydration
+(hydrate (Order 1) :customer :shipping [:order-items :store_item])
+
+(hydrate {:store_item_id 2} :store_item)
+
+(toucan.hydrate/flush-hydration-key-caches!)
 
 (def worker (future (kafka/run-application "localhost:9092" 
                                            "dbserver1.inventory.customers" 
